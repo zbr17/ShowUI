922c292b8a60ac64d7bd723fb5a3516a0ac9172a
Kevin <kevin.qh.lin@gmail.com>
2024-12-18 19:46:49 +0800

    Update display_uigraph.py

--- display_uigraph.py
+++ display_uigraph.py
@@ -10,40 +10,37 @@ max_pixels = 1344*28*28
 
 processor = ShowUIProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
-# The dir to load images for ui_graph preprocessing
-img_dir = 'images'
-img_list = os.listdir(img_dir)
-
+# The input images for ui_graph preprocessing
+img_url = 'examples/0730d43001da36204b8cb9495b61308.png'
 # The dir to save images with ui_graph visualization
-vis_dir = 'display'
+vis_dir = '.'
 
-for img_url in img_list:
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image",
-                    "image": os.path.join(img_dir, img_url),
-                    "min_pixels": min_pixels,
-                    "max_pixels": max_pixels,
-                },
-            ],
-        }
-    ]
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": img_url,
+                "min_pixels": min_pixels,
+                "max_pixels": max_pixels,
+            },
+        ],
+    }
+]
 
-    # Preparation for inference
-    text = processor.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True
-    )
-    image_inputs, video_inputs = process_vision_info(messages)
-    inputs = processor(
-        text=[text],
-        images=image_inputs,
-        videos=video_inputs,
-        padding=True,
-        return_tensors="pt",
-        ui_graph=True,
-        ui_graph_threshold=1,
-        ui_graph_vis_dir=vis_dir,
-    )
+# Preparation for inference
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+image_inputs, video_inputs = process_vision_info(messages)
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+    ui_graph=True,
+    ui_graph_threshold=1,
+    ui_graph_vis_dir=vis_dir,
+)
